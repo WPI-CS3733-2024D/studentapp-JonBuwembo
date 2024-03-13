f4764651d1b64ac88a96570a39673eb2f9583824
JonBuwembo <122390422+JonBuwembo@users.noreply.github.com>
2024-03-13 18:50:39 -0400

    Version v.03 - Added and styled a Create Class form.

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import  ValidationError, Length
+from wtforms.validators import  ValidationError, Length, DataRequired
 from app.models import Class, Major
+from wtforms_sqlalchemy.fields import QuerySelectField
+
+# Forms handle submitted data by the user.
+# when the user interacts with the web browser and
+# submits information online, a form is also activated and submitted.
+
+# Examples where forms are used: 
+#   1. Ordering a product online, 
+#   2. filtering custom views, 
+#   3. gathering text, 
+#   4. updating your information online, 
+#   5. collecting feedback
+
+# QUERY SELECT FIELD: allows us to get data from database and dynamically populate options in the dropdown list.
+def get_major():
+    return Major.query.all()
+
+def get_majorlabel(theMajor):
+    return theMajor.name
 
 class ClassForm(FlaskForm):
     coursenum = StringField('Course Number',[Length(min=3, max=3)])
+    title = StringField('Course Title', validators=[DataRequired()]) # textbox
+    major = QuerySelectField('Majors', query_factory = get_major, get_label = get_majorlabel, allow_blank=False) # drop down feature to select major
     submit = SubmitField('Post')
+
--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,15 @@ def index():
     allclasses = Class.query.order_by(Class.major).all()
     return render_template('index.html', title="Course List", classes = allclasses)
 
+@app.route('/createclass/', methods=['GET', 'POST'])
+def createclass():
+    cform = ClassForm()
+    # handle rendering of the form
+    if cform.validate_on_submit():
+        newClass = Class(coursenum = cform.coursenum.data, title = cform.title.data, major = cform.major.data.name)
+        db.session.add(newClass)
+        db.session.commit()
+
+        flash('Class "' + newClass.major + ' - ' + newClass.coursenum +'" is created')
+        return redirect(url_for('index'))
+    return render_template('create_class.html', form = cform)
