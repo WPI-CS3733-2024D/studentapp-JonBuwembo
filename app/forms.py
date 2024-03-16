cb50805dea6cdcb1b4284e319978c4872ede417a
JonBuwembo <122390422+JonBuwembo@users.noreply.github.com>
2024-03-16 13:46:31 -0400

    Version v.07 - Added the edit profile page.

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, TextAreaField, PasswordField, Bool
 from wtforms.validators import  ValidationError, Length, DataRequired, Email, EqualTo
 from app.models import Class, Major, Student
 from wtforms_sqlalchemy.fields import QuerySelectField
+from flask_login import current_user
 
 # Forms handle submitted data by the user.
 # when the user interacts with the web browser and
@@ -53,3 +54,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class EditForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    address = TextAreaField('Address', [Length(min=0, max=200)]) # TextAreaField makes an adjustedable textbox
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')]) # make sure passwords are the same
+    submit = SubmitField('Submit')
+
+    def validate_email(self, email):
+        students = Student.query.filter_by(email = email.data).all()
+
+        for student in students: 
+            if (student.id != current_user.id):
+                raise ValidationError('The email is associated with another account! Use a different email address')
